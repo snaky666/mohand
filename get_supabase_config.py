dc7c1b96de9edf2c2d3d7a17f8694df353b94f8e
snaky666 <madimoh44@gmail.com>
2025-10-20 19:18:13 +0100

    Update get_supabase_config.py

--- get_supabase_config.py
+++ get_supabase_config.py
@@ -6,8 +6,8 @@ import json
 def get_config():
     """Get Supabase configuration from environment variables."""
     return {
-        'url': os.environ.get('SUPABASE_URL', ''),
-        'key': os.environ.get('SUPABASE_ANON_KEY', '')
+        'url': os.environ.get('https://uqirjzszhxgqecdinuot.supabase.co', ''),
+        'key': os.environ.get('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxaXJqenN6aHhncWVjZGludW90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5Njk0NDksImV4cCI6MjA3NjU0NTQ0OX0.BPYKL2TaeIZayOPdI7-E0OHM3FuCZP15bsfPNSj_Zjk', '')
     }
 
 if __name__ == '__main__':
